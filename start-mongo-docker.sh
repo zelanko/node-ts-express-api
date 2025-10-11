effabc5e6ccb0562c4239fafa10fb09362637bb6
Ari Zelanko <zelanko@yahoo.com>
2025-10-11 15:15:02 -0400

    remove start-mongo-docker.sh in favor of the new compose.yaml

--- start-mongo-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-podman run \
-  -detach \
-  --name mongodb \
-  --replace \
-  --publish 27017:27017 \
-  --volume /home/ari-zelanko/data/mongodb:/data/db:rw \
-  docker.io/mongodb/mongodb-community-server:latest
